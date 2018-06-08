76c10c33a1a49a8e074e1640e673b4d10c6a63bb
sujianyu <230677mbh>
2018-06-08 16:02:12 +0800

    new

--- Bikeshare.py
+++ Bikeshare.py
@@ -23,10 +23,10 @@ print(daydata.head())
 #将数据分为2011年和2012年两组数据
 _2011data = daydata.loc[daydata.yr == 0]
 _2012data = daydata.loc[daydata.yr == 1]
-X_train = _2011data.drop(['cnt', 'dteday', 'yr'], axis=1)
+X_train = _2011data.drop(['cnt', 'dteday', 'yr', 'casual', 'registered'], axis=1)
 y_train = _2011data["cnt"]
 
-X_test = _2012data.drop(['cnt', 'dteday', 'yr'], axis=1)
+X_test = _2012data.drop(['cnt', 'dteday', 'yr', 'casual', 'registered'], axis=1)
 y_test = _2012data["cnt"]
 print(X_train.shape)
 print(y_train.shape)
@@ -56,9 +56,6 @@ lr.fit(X_train, y_train)
 y_test_pred_lr = lr.predict(X_test)
 y_train_pred_lr = lr.predict(X_train)
 
-print(lr.coef_.T)
-
-
 # 看看各特征的权重系数，系数的绝对值大小可视为该特征的重要性
 fs = pd.DataFrame({"columns": list(columns), "coef": list((lr.coef_.T))})
 fs.sort_values(by=['coef'], ascending=False)
@@ -71,7 +68,7 @@ print('The r2 score of LinearRegression on train is', r2_score(y_train, y_train_
 #在训练集上观察预测残差的分布，看是否符合模型假设：噪声为0均值的高斯噪声
 f, ax = plt.subplots(figsize=(7, 5))
 f.tight_layout()
-ax.hist(y_train - y_train_pred_lr,bins=40, label='Residuals Linear', color='b', alpha=.5)
+ax.hist(y_train - y_train_pred_lr, bins=40, label='Residuals Linear', color='b', alpha=.5)
 ax.set_title("Histogram of Residuals")
 ax.legend(loc='best')
 
@@ -108,7 +105,6 @@ mse_mean = np.mean(ridge.cv_values_, axis = 0)
 plt.plot(np.log10(alphas), mse_mean.reshape(len(alphas),1))
 
 #这是为了标出最佳参数的位置，不是必须
-#plt.plot(np.log10(ridge.alpha_)*np.ones(3), [0.28, 0.29, 0.30])
 
 plt.xlabel('log(alpha)')
 plt.ylabel('mse')
@@ -119,15 +115,14 @@ print ('alpha is:', ridge.alpha_)
 # 看看各特征的权重系数，系数的绝对值大小可视为该特征的重要性
 fs = pd.DataFrame({"columns":list(columns), "coef_lr":list((lr.coef_.T)), "coef_ridge":list((ridge.coef_.T))})
 fs.sort_values(by=['coef_lr'],ascending=False)
+print(fs)
 
 # Lasso／L1正则
 from sklearn.linear_model import LassoCV
 
 #设置超参数搜索范围
-#alphas = [ 0.01, 0.1, 1, 10,100]
 
 #生成一个LassoCV实例
-#lasso = LassoCV(alphas=alphas)
 lasso = LassoCV()
 
 #训练（内含CV）
@@ -143,7 +138,6 @@ print ('The r2 score of LassoCV on test is', r2_score(y_test, y_test_pred_lasso)
 print ('The r2 score of LassoCV on train is', r2_score(y_train, y_train_pred_lasso))
 mses = np.mean(lasso.mse_path_, axis=1)
 plt.plot(np.log10(lasso.alphas_), mses)
-# plt.plot(np.log10(lasso.alphas_)*np.ones(3), [0.3, 0.4, 1.0])
 plt.xlabel('log(alpha)')
 plt.ylabel('mse')
 plt.show()
@@ -158,7 +152,6 @@ fs.sort_values(by=['coef_lr'], ascending=False)
 
 mses = np.mean(lasso.mse_path_, axis=1)
 plt.plot(np.log10(lasso.alphas_), mses)
-# plt.plot(np.log10(lasso.alphas_)*np.ones(3), [0.3, 0.4, 1.0])
 plt.xlabel('log(alpha)')
 plt.ylabel('mse')
 plt.show()
